95a7265829de629a716a89286fbeab5349a23cf3
haoxiaoyong <hxyHelloWorld@163.com>
2017-10-25 11:00:55 +0800

    hello-sv-JWT

---
+++ common/JWT/src/main/java/com/huluwa/cc/JWTSend.java
@@ -0,0 +1,26 @@
+package com.huluwa.cc;
+
+import com.huluwa.cc.jwt.ProjectToken;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Administrator on 2017/10/24.
+ */
+@Configuration
+//@SpringBootApplication(scanBasePackages = {"com.huluwa"})
+@ServletComponentScan(basePackages = {"com.huluwa"})
+public class JWTSend {
+
+        public static void main(String[] args) {
+            SpringApplication.run(JWTSend.class, args);
+        }
+        @Bean
+        public ProjectToken getProjectToken() {
+            ProjectToken projectToken = new ProjectToken();
+            return projectToken;
+        }
+}
